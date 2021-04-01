ab0b494a6260b6bb774c4387f0f0d2d8c157e7e7
salkorad <salko.radovovic@gmail.com>
2021-04-02 01:56:12 +0200

    Fetch - DELETE

---
+++ ex10/src/DeleteRequest.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class DeleteRequest extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { status: null };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/posts/1", { method: "Delete" })
+    .then(() => this.setState({ status: "Delete succesful" }));
+  }
+  render() {
+    const { status } = this.state;
+    return (
+      <div className="card text-center m-3">
+        <h5 className="card-header">Simple DELETE Request</h5>
+        <div className="card-body">Status: {status}</div>
+      </div>
+    );
+  }
+}
+export default DeleteRequest;
